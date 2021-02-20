cdab340367fea8ea37ff5078bc27750484a0c48b
Ran You <ran.you8311@gmail.com>
2021-02-20 09:05:54 -0800

    complete my blog with joy

--- app.js
+++ app.js
@@ -8,46 +8,48 @@ const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui 
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 const app = express();
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/postDB', {useNewUrlParser: true, useUnifiedTopology: true});
+const postSchema = new mongoose.Schema({
+  title: String,
+  post: String
+});
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+let Post=mongoose.model('Post',postSchema)
 let posts=[]
 
 app.post('/compose', (req,res)=>{
   const title=req.body.publish_box
   const post=req.body.postBody
-  const postFull={title:title,post:post}
-  posts.push(postFull)
-  
-  res.redirect('/')
+  const postFull=new Post({title:title,post:post})
+  postFull.save((err)=>{
+    if (!err){
+        res.redirect('/')
+    }
+  })
+ 
+
 })
 
+app.get('/', (req,res)=>{
+   Post.find({},(err,posts)=>{
+  res.render(__dirname+'/views/home.ejs',{homeStartingContent:homeStartingContent,posts:posts})
+})
+})
 
 
 app.get('/posts/:postId', function (req, res) {
 const postId=req.params.postId
-const postTitles=posts.map((post)=>{
-  const postTitle=post.title
-  if (_.lowerCase(postTitle)===_.lowerCase(postId)){
-    console.log('found')
-    res.render(__dirname+'/views/post.ejs',{title:post.title,post:post.post})
-  } else{
-    console.log('not a match')
-    res.redirect('/')
-  }
+Post.findById(postId,(err,foundPost)=>{
+    res.render(__dirname+'/views/post.ejs',{title:foundPost.title,post:foundPost.post})
+  } )
 })
-  
-})
+ 
 
-
-
-
-
-app.get('/', (req,res)=>{
-  res.render(__dirname+'/views/home.ejs',{homeStartingContent:homeStartingContent,posts:posts})
-})
 
 app.get('/about', (req,res)=>{
   res.render(__dirname+'/views/about.ejs',{aboutContent:aboutContent})
@@ -63,4 +65,4 @@ app.get('/compose', (req,res)=>{
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
-});
+})
